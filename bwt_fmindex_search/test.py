a48075c85ea263122465f2ca77ed0e0860e2d41e
TheoPet <teodora.petrovic.best@gmail.com>
2018-05-17 22:11:00 +0200

    Test do FMIndex_create

--- bwt_fmindex_search/test.py
+++ bwt_fmindex_search/test.py
@@ -52,30 +52,30 @@ assert set(suffix_array_manber_myers(text_with_blancs)) == set([7, 4, 9, 0, 8, 1
 
 #Test BWT
 
-text_normal_bwt_1 = "abracadabra$"
+text_normal_bwt_1 = "abracadabra"+'\0'
 suffix_array_normal_1 = suffix_array_manber_myers(text_normal_bwt_1)
-assert (bw_transform(text_normal_bwt_1, suffix_array_normal_1)) == "ard$rcaaaabb"
+assert (bw_transform(text_normal_bwt_1, suffix_array_normal_1)) == "ard"+'\0'"rcaaaabb"
 
 
-text_normal_bwt_2 = "banana$"
+text_normal_bwt_2 = "banana"+'\0'
 suffix_array_normal_2 = suffix_array_manber_myers(text_normal_bwt_2)
-assert (bw_transform(text_normal_bwt_2, suffix_array_normal_2)) == "annb$aa"
+assert (bw_transform(text_normal_bwt_2, suffix_array_normal_2)) == "annb"+'\0'"aa"
 
-text_bwt_repetitive = "nanananananinininenene$"
+text_bwt_repetitive = "nanananananinininenene"+'\0'
 suffix_array_repetitive = suffix_array_manber_myers(text_bwt_repetitive)
-assert (bw_transform(text_bwt_repetitive, suffix_array_repetitive)) == "ennnnnnnnnnn$aaaaeeiiia"
+assert (bw_transform(text_bwt_repetitive, suffix_array_repetitive)) == "ennnnnnnnnnn"+'\0'"aaaaeeiiia"
 
-text_bwt_empty = "$"
+text_bwt_empty = '\0'
 suffix_array_empty = suffix_array_manber_myers(text_bwt_empty)
-assert (bw_transform(text_bwt_empty, suffix_array_empty)) == "$"
+assert (bw_transform(text_bwt_empty, suffix_array_empty)) == '\0'
 
-text_bwt_num = "aaba4bba2$"
+text_bwt_num = "aaba4bba2"+'\0'
 suffix_array_num = suffix_array_manber_myers(text_bwt_num)
-assert (bw_transform(text_bwt_num, suffix_array_num)) == "2aabb$aba4"
+assert (bw_transform(text_bwt_num, suffix_array_num)) == "2aabb"+'\0'"aba4"
 
-text_bwt_blancs = "This is a text$"
+text_bwt_blancs = "This is a text"+'\0'
 suffix_array_blancs = suffix_array_manber_myers(text_bwt_blancs)
-assert (bw_transform(text_bwt_blancs, suffix_array_blancs)) == "ssat$ tT hiix e"
+assert (bw_transform(text_bwt_blancs, suffix_array_blancs)) == "tssa" + '\0' + " tT hiix e"
 
 #Test helper functions
 
@@ -86,10 +86,10 @@ assert(count_characters(text_with_blancs)) == (Counter({' ': 3, 'i': 2, 's': 2, 
                                                         'h': 1, 'T': 1, 'x': 1}))
 
 #terminate string
-assert(terminate_string(text_normal_1)) == "abracadabra$"
-assert(terminate_string(text_with_blancs)) == "This is a text$"
-assert(terminate_string(text_empty)) == "$"
-assert(terminate_string(text_bwt_num)) == "aaba4bba2$"
+assert(terminate_string(text_normal_1)) == "abracadabra"+'\0'
+assert(terminate_string(text_with_blancs)) == "This is a text"+'\0'
+assert(terminate_string(text_empty)) == '\0'
+assert(terminate_string(text_bwt_num)) == "aaba4bba2"+'\0'
 
 #calculate ranks
 assert(calculate_ranks(text_normal_1)) == [0, 0, 0, 1, 0, 2, 0, 3, 1, 1, 4]
@@ -109,20 +109,102 @@ count_text_empty = count_characters(text_bwt_empty)
 count_text_repetitive = count_characters(text_bwt_repetitive)
 count_text_blancs = count_characters(text_bwt_blancs)
 
-assert(calculate_first_occurrences(count_normal_text_1)) == {'$': 0, 'a': 1, 'b': 6, 'c': 8, 'd': 9, 'r': 10}
-assert(calculate_first_occurrences(count_normal_text_2)) == {'$': 0, 'a': 1, 'b': 4, 'n': 5}
-assert(calculate_first_occurrences(count_text_num)) == {'$': 0, '2': 1, '4': 2, 'a': 3, 'b': 7}
-assert(calculate_first_occurrences(count_text_empty)) == {'$': 0}
-assert(calculate_first_occurrences(count_text_repetitive)) == {'$': 0, 'a': 1, 'e': 6, 'i': 9, 'n': 12}
-assert(calculate_first_occurrences(count_text_blancs)) == {'$': 0}
+assert(calculate_first_occurrences(count_normal_text_1)) == {'\0': 0, 'a': 1, 'b': 6, 'c': 8, 'd': 9, 'r': 10}
+assert(calculate_first_occurrences(count_normal_text_2)) == {'\0': 0, 'a': 1, 'b': 4, 'n': 5}
+assert(calculate_first_occurrences(count_text_num)) == {'\0': 0, '2': 1, '4': 2, 'a': 3, 'b': 7}
+assert(calculate_first_occurrences(count_text_empty)) == {'\0': 0}
+assert(calculate_first_occurrences(count_text_repetitive)) == {'\0': 0, 'a': 1, 'e': 6, 'i': 9, 'n': 12}
+assert(calculate_first_occurrences(count_text_blancs)) == {'\0': 0, ' ': 1, 'T': 4, 'a': 5, 'e': 6, 'h': 7, 'i': 8,
+                                                           's': 10, 't': 12, 'x': 14}
 
+#Test f_column creation
+f_column_text_normal_1 = create_f_column(text_normal_1+'\0')
+f_column_text_normal_2 = create_f_column(text_normal_2+'\0')
+f_column_text_num = create_f_column(text_with_num+'\0')
+f_column_text_empty = create_f_column(text_empty+'\0')
+f_column_text_repetitive = create_f_column(text_repetitive+'\0')
+f_column_text_blancs = create_f_column(text_with_blancs+'\0')
+
+#Khm, nisam sigurna da je bas dobro :(
+assert f_column_text_normal_1._count == Counter({'\0': 1, 'a': 5, 'b': 2, 'c': 1, 'd': 1, 'r': 2})
+assert f_column_text_normal_1._first_occurrence == {'\0': 0, 'a': 1, 'b': 6, 'c': 8, 'd': 9, 'r': 10}
+
+assert f_column_text_normal_2._count == Counter({'\0': 1, 'a': 3, 'b': 1, 'n': 2})
+assert f_column_text_normal_2._first_occurrence == {'\0': 0, 'a': 1, 'b': 4, 'n': 5}
+
+assert f_column_text_num._count == Counter({'\0': 1, '2': 1, '4': 1, 'a': 4, 'b': 3})
+assert f_column_text_num._first_occurrence == {'\0': 0, '2': 1, '4': 2, 'a': 3, 'b': 7}
+
+assert f_column_text_empty._count == Counter({'\0': 1})
+assert f_column_text_empty._first_occurrence == {'\0': 0}
+
+assert f_column_text_repetitive._count == Counter({'\0': 1, 'a': 5, 'e': 3, 'i': 3, 'n': 11})
+assert f_column_text_repetitive._first_occurrence == {'\0': 0, 'a': 1, 'e': 6, 'i': 9, 'n': 12}
+
+assert f_column_text_blancs._count == Counter({'\0': 1, ' ': 3, 'i': 2, 's': 2, 't': 2, 'a': 1, 'e': 1,
+                                               'h': 1, 'T': 1, 'x': 1})
+assert f_column_text_blancs._first_occurrence == {'\0': 0, ' ': 1, 'T': 4, 'a': 5, 'e': 6, 'h': 7, 'i': 8, 's': 10,
+                                                  't': 12, 'x': 14}
+
+
+#Test FColumn.get_first_occurence
+assert f_column_text_normal_1.get_first_occurrence('a') == 1
+assert f_column_text_num.get_first_occurrence('4') == 2
+assert f_column_text_empty.get_first_occurrence('\0') == 0
+assert f_column_text_repetitive.get_first_occurrence('u') == 0 #ako je karakter nepostojeci, da li je ok da vraca  0???
+assert f_column_text_blancs.get_first_occurrence(' ') == 1
+
+#Test FColumn.char_range
+assert f_column_text_normal_1.char_range('r') == (10, 12)
+assert f_column_text_normal_2.char_range('b') == (4, 5)
+assert f_column_text_empty.char_range('a') == (0, 0)
+assert f_column_text_repetitive.char_range('n') == (12, 23)
+assert f_column_text_repetitive.char_range('e') == (6, 9)
+assert f_column_text_blancs.char_range('T') == (4, 5)
+assert f_column_text_blancs.char_range(' ') == (1, 4)
+assert f_column_text_blancs.char_range('\0') == (0, 11)
+
+#Test create_fm_index
+fm_index_1 = create_fm_index(text_normal_1)
+fm_index_2 = create_fm_index(text_normal_2)
+fm_index_repetitive = create_fm_index(text_repetitive)
+fm_index_empty = create_fm_index(text_empty)
+fm_index_num = create_fm_index(text_with_num)
+fm_index_blancs = create_fm_index(text_with_blancs)
+
+assert fm_index_1.bwt == "ard"+'\0'"rcaaaabb"
+assert fm_index_1.sa == "annb"+'\0'"aa"
+assert fm_index_1.ranks ==
+assert fm_index_1._f_column ==
+
+assert fm_index_2.bwt == "annb"+'\0'"aa"
+assert fm_index_2.sa ==
+assert fm_index_2.ranks ==
+assert fm_index_2._f_column ==
+
+assert fm_index_repetitive.bwt == "ennnnnnnnnnn"+'\0'"aaaaeeiiia"
+assert fm_index_repetitive.sa ==
+assert fm_index_repetitive.ranks ==
+assert fm_index_repetitive._f_column ==
+
+assert fm_index_empty.bwt == '\0'
+assert fm_index_empty.sa ==
+assert fm_index_empty.ranks ==
+assert fm_index_empty._f_column ==
+
+assert fm_index_num.bwt == "2aabb"+'\0'"aba4"
+assert fm_index_num.sa ==
+assert fm_index_num.ranks ==
+assert fm_index_num._f_column ==
+
+assert fm_index_blancs.bwt == "tssa" + '\0' + " tT hiix e"
+assert fm_index_blancs.sa ==
+assert fm_index_blancs.ranks ==
+assert fm_index_blancs._f_column ==
+
+
+#Test FMIndex._find_preceders
 if __name__ == "__main__":
- sort_bucket_test = [1, 5, 7, 8, 9, 5]
- #sort_bucket(sort_bucket_test, [1, 2])
-text_normal_bwt_num = "aaba4bb2$"
-#suffix_array_num = suffix_array_manber_myers(text_normal_bwt_num)
-print(count_characters(text_normal_bwt_num))
-#print (calculate_ranks((text_normal_bwt_num)))
-print (calculate_first_occurrences(count_characters(text_normal_bwt_num)))
-
-#print (bw_transform(text_normal_bwt_num, suffix_array_num))
+    text_normal = "abracadabra"
+    f_column = create_f_column(text_normal);
+    print(f_column.get_first_occurrence('a'))
